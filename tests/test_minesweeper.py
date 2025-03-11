eab34b18da47d3e58267fddadc289047a1e90983
Astrid Chatin <astridchatin@gmail.com>
2025-03-11 17:20:59 +0100

    Initial commit

--- tests/test_minesweeper.py
+++ tests/test_minesweeper.py
@@ -1,8 +1,11 @@
 import pytest
+
 import src.minesweeper.minesweeper as minesweeper
+
 
 def test_module_exists():
     assert minesweeper
+
 
 def test_place_mines():
     game = minesweeper.Minesweeper(3, 3, 2)
